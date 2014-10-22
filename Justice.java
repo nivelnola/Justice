11312cb3dcf951d15fcfc2779439c120c51a10a9
Alon Levin <alon.s.levin@gmail.com>
2014-10-22 09:48:00 -0400

    Main file - got rid of extra stuff

--- Justice.java
+++ Justice.java
@@ -1,39 +1,17 @@
-public class Driver {
+//Imports
+import java.util.*;
+
+public class Justice {
     //Methods
     public static void main(String[] args) {
-	Vigilante a = new Vigilante("Vig 1");
-	Consigliere b = new Consigliere("Consig 1");
-	System.out.println("###CHARACTER CREATION###");
-	System.out.println(a);
-	System.out.println(b);
-	Vigilante c = new Vigilante("Vig 2");
-	Consigliere d = new Consigliere("Consig 2");
-	System.out.println("###CONSIG TESTS###");
-	b.check();
-	b.search(c);
-	b.search(d);
-	b.check();
-	System.out.println("###ATTACK TESTS###");
-	a.attack(c);
-	b.attack(d);
-	System.out.println(c);
-	System.out.println(d);
-	System.out.println("###VIG TESTS###");
-	a.suicide();
-	a.fire(b);
-	a.fire(d);
-	a.fire(b);
-	a.fire(c);
-	c.fire(a);
-	c.suicide();
-	System.out.println(a);
-	System.out.println(b);
-	System.out.println(c);
-	System.out.println(d);
-	System.out.println("###ISALIVE TESTS###");
-	a.isAlive();
-	b.isAlive();
-        c.isAlive();
-	d.isAlive();
+	Scanner s = new Scanner(System.in);
+	String job;
+	String nam;
+	System.out.println("Welcome to Salem! You are a newcomer to the this town.");
+	System.out.println("What is your job? You can be a Mafioso, a Detective, a Vigilante, an Executioner, or a Survivor.\n");
+	job = s.nextLine();
+	System.out.println("What is your name?");
+	nam = s.nextLine();
+	System.out.println("Job: " + job + " | Name: " + nam);
     }
 }
